eb9003cb335d4eff3a03ee6a4437a9f7742af5f2
Logan Ballard <loganballard@gmail.com>
2025-09-26 14:07:35 -0400

    adding unit tests and ability for pod to be considered

--- pkg/loosebinpack/loosebinpack.go
+++ pkg/loosebinpack/loosebinpack.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
@@ -21,16 +22,18 @@ const Name = "LooseBinPack"
 
 var (
 	ErrNodeNotFound             = errors.New("node not found")
+	ErrPodNotFound              = errors.New("pod not found")
 	ErrInvalidArgs              = errors.New("invalid args")
 	ErrCpuUtilizationTooHigh    = errors.New("cpu utilization too high")
 	ErrMemoryUtilizationTooHigh = errors.New("memory utilization too high")
+	ErrPodNoContainers          = errors.New("pod has no containers")
 )
 
 var _ = framework.FilterPlugin(&LooseBinPack{})
 
 func New(ctx context.Context, obj runtime.Object, h framework.Handle) (framework.Plugin, error) {
 	logger := klog.FromContext(ctx).WithValues("plugin", Name)
-	logger.V(4).Info("Creating new instance of the NetworkOverhead plugin")
+	logger.V(4).Info("Creating loosebinpack plugin")
 
 	args, ok := obj.(*config.LooseBinPackArgs)
 	if !ok {
@@ -49,20 +52,32 @@ func (lbp *LooseBinPack) Name() string {
 	return Name
 }
 
-func (lbp *LooseBinPack) Filter(ctx context.Context, _ *framework.CycleState, _ *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+func (lbp *LooseBinPack) Filter(ctx context.Context, _ *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
 	if nodeInfo.Node() == nil {
 		lbp.logger.V(4).Error(ErrNodeNotFound, ErrNodeNotFound.Error())
 		return framework.NewStatus(framework.Error, ErrNodeNotFound.Error())
 	}
 
-	nodeAllocatableCpu := nodeInfo.Allocatable.MilliCPU
-	nodeAllocatableMemory := nodeInfo.Allocatable.Memory
+	if pod == nil {
+		lbp.logger.V(4).Error(ErrPodNotFound, ErrPodNotFound.Error())
+		return framework.NewStatus(framework.Error, ErrPodNotFound.Error())
+	}
 
-	nodeRequestedCpu := nodeInfo.Requested.MilliCPU
-	nodeRequestedMemory := nodeInfo.Requested.Memory
+	if len(pod.Spec.Containers) == 0 {
+		lbp.logger.V(4).Error(ErrPodNoContainers, ErrPodNoContainers.Error())
+		return framework.NewStatus(framework.Error, ErrPodNoContainers.Error())
+	}
 
-	nodeCpuUtilization := float64(nodeRequestedCpu) / float64(nodeAllocatableCpu)
-	nodeMemoryUtilization := float64(nodeRequestedMemory) / float64(nodeAllocatableMemory)
+	nodeRequestedCpuAfterPod := nodeInfo.Requested.MilliCPU
+	nodeRequestedMemoryAfterPod := nodeInfo.Requested.Memory
+
+	for _, container := range pod.Spec.Containers {
+		nodeRequestedCpuAfterPod += container.Resources.Requests.Cpu().ScaledValue(resource.Milli)
+		nodeRequestedMemoryAfterPod += container.Resources.Requests.Memory().Value()
+	}
+
+	nodeCpuUtilization := float64(nodeRequestedCpuAfterPod) / float64(nodeInfo.Allocatable.MilliCPU)
+	nodeMemoryUtilization := float64(nodeRequestedMemoryAfterPod) / float64(nodeInfo.Allocatable.Memory)
 
 	if nodeCpuUtilization > float64(lbp.cpuThresholdPercent) {
 		lbp.logger.V(4).Error(ErrCpuUtilizationTooHigh, ErrCpuUtilizationTooHigh.Error(), "node", klog.KObj(nodeInfo.Node()))
--- pkg/loosebinpack/loosebinpack_test.go
+++ pkg/loosebinpack/loosebinpack_test.go
@@ -3,10 +3,12 @@ package loosebinpack
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 	"sigs.k8s.io/scheduler-plugins/apis/config"
@@ -25,13 +27,23 @@ func TestNew(t *testing.T) {
 func TestNodeFilter(t *testing.T) {
 	tt := []struct {
 		name                   string
+		pod                    *v1.Pod
 		cpuThresholdPercent    int64
 		memoryThresholdPercent int64
 		nodeInfo               *framework.NodeInfo
 		expectedStatus         framework.Status
 	}{
 		{
+			name:                   "pod with no containers",
+			pod:                    makePod("pod1"),
+			cpuThresholdPercent:    85,
+			memoryThresholdPercent: 85,
+			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 100, 100),
+			expectedStatus:         *framework.NewStatus(framework.Error, ErrPodNoContainers.Error()),
+		},
+		{
 			name:                   "node with cpu and memory utilization below threshold",
+			pod:                    makePod("pod1", containerReq{CpuReq: 100, MemoryReq: 100}),
 			cpuThresholdPercent:    85,
 			memoryThresholdPercent: 85,
 			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 100, 100),
@@ -39,46 +51,43 @@ func TestNodeFilter(t *testing.T) {
 		},
 		{
 			name:                   "node with cpu and memory utilization above threshold",
+			pod:                    makePod("pod1", containerReq{CpuReq: 100, MemoryReq: 100}),
 			cpuThresholdPercent:    85,
 			memoryThresholdPercent: 85,
-			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 1000, 1000),
-			expectedStatus:         *framework.NewStatus(framework.Unschedulable),
+			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 900, 900),
+			expectedStatus:         *framework.NewStatus(framework.Unschedulable, ErrCpuUtilizationTooHigh.Error()),
 		},
 		{
-			name:                   "node with memory utilization above threshold",
+			name:                   "node with cpu and memory utilization below threshold until pod is added",
+			pod:                    makePod("pod1", containerReq{CpuReq: 100, MemoryReq: 100}),
 			cpuThresholdPercent:    85,
 			memoryThresholdPercent: 85,
-			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 100, 1000),
-			expectedStatus:         *framework.NewStatus(framework.Unschedulable),
+			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 800, 800),
+			expectedStatus:         *framework.NewStatus(framework.Unschedulable, ErrCpuUtilizationTooHigh.Error()),
 		},
 		{
-			name:                   "node with cpu utilization above threshold",
+			name:                   "node with cpu and memory utilization below threshold until pod is added - two containers. first would not cause failure, second will",
+			pod:                    makePod("pod1", containerReq{CpuReq: 100, MemoryReq: 100}, containerReq{CpuReq: 100, MemoryReq: 100}),
 			cpuThresholdPercent:    85,
 			memoryThresholdPercent: 85,
-			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 1000, 100),
-			expectedStatus:         *framework.NewStatus(framework.Unschedulable),
+			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 700, 700),
+			expectedStatus:         *framework.NewStatus(framework.Unschedulable, ErrCpuUtilizationTooHigh.Error()),
 		},
 		{
-			name:                   "node with different thresholdes, both pass",
-			cpuThresholdPercent:    50,
-			memoryThresholdPercent: 75,
-			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 100, 100),
+			name:                   "node with cpu and memory utilization below threshold - two containers. first would not cause failure, second wont either",
+			pod:                    makePod("pod1", containerReq{CpuReq: 100, MemoryReq: 100}, containerReq{CpuReq: 100, MemoryReq: 100}),
+			cpuThresholdPercent:    85,
+			memoryThresholdPercent: 85,
+			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 600, 600),
 			expectedStatus:         *framework.NewStatus(framework.Success),
 		},
 		{
-			name:                   "node with different thresholdes, CPU too high",
-			cpuThresholdPercent:    50,
-			memoryThresholdPercent: 75,
-			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 501, 100),
-			expectedStatus:         *framework.NewStatus(framework.Unschedulable),
-		},
-
-		{
-			name:                   "node with different thresholdes, Memory too high",
-			cpuThresholdPercent:    50,
-			memoryThresholdPercent: 75,
-			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 499, 751),
-			expectedStatus:         *framework.NewStatus(framework.Unschedulable),
+			name:                   "node with cpu and memory utilization below threshold - two containers. first would not cause failure, second will cause CPU threshold failure",
+			pod:                    makePod("pod1", containerReq{CpuReq: 100, MemoryReq: 100}, containerReq{CpuReq: 151, MemoryReq: 100}),
+			cpuThresholdPercent:    85,
+			memoryThresholdPercent: 85,
+			nodeInfo:               makeNodeInfo("node1", 1000, 1000, 600, 600),
+			expectedStatus:         *framework.NewStatus(framework.Unschedulable, ErrCpuUtilizationTooHigh.Error()),
 		},
 	}
 
@@ -86,9 +95,43 @@ func TestNodeFilter(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			filterPlugin, err := makePlugin(t.Context(), tc.cpuThresholdPercent, tc.memoryThresholdPercent)
 			assert.NoError(t, err)
-			status := filterPlugin.Filter(t.Context(), nil, nil, tc.nodeInfo)
+			status := filterPlugin.Filter(t.Context(), nil, tc.pod, tc.nodeInfo)
 			assert.Equal(t, tc.expectedStatus.Code(), status.Code())
+			assert.Equal(t, tc.expectedStatus.Message(), status.Message())
 		})
+	}
+}
+
+type containerReq struct {
+	CpuReq    int64
+	MemoryReq int64
+}
+
+func makeContainer(name string, cpuReq, memoryReq int64) v1.Container {
+	return v1.Container{
+		Name: name,
+		Resources: v1.ResourceRequirements{
+			Requests: v1.ResourceList{
+				v1.ResourceCPU:    resource.MustParse(strconv.FormatInt(cpuReq, 10) + "m"),
+				v1.ResourceMemory: resource.MustParse(strconv.FormatInt(memoryReq, 10) + "Ki"),
+			},
+		},
+	}
+}
+
+func makePod(name string, containerReq ...containerReq) *v1.Pod {
+	containers := make([]v1.Container, len(containerReq))
+	for i, req := range containerReq {
+		containers[i] = makeContainer(fmt.Sprintf("%s-%d", name, i), req.CpuReq, req.MemoryReq)
+	}
+
+	return &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: v1.PodSpec{
+			Containers: containers,
+		},
 	}
 }
 
@@ -101,11 +144,11 @@ func makeNodeInfo(name string, allocatableCpu, allocatableMemory, requestedCpu, 
 	})
 	n.Allocatable = &framework.Resource{
 		MilliCPU: allocatableCpu,
-		Memory:   allocatableMemory,
+		Memory:   allocatableMemory * 1024,
 	}
 	n.Requested = &framework.Resource{
 		MilliCPU: requestedCpu,
-		Memory:   requestedMemory,
+		Memory:   requestedMemory * 1024,
 	}
 	return n
 }
